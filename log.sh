e959d55406c83b91d8e285799a63f6079fde8502
tuantuan.lv <dangoakachan@foxmail.com>
2012-09-26 16:22:51 +0800

    add more setting functions

--- log.sh
+++ log.sh
@@ -7,75 +7,75 @@
 # Description: defines functions for printing log messages.
 #
 
-# Show log whose level less than this
-log_level=3
+# {{ Global constants definition
 
-# Debug log constants
+# Log level constants
 LOG_ERROR=0       # Level error
 LOG_WARNING=1     # Level warning
 LOG_INFO=2        # Level info
 LOG_DEBUG=3       # Level debug
 
-# Color escape string
-COLOR_RED="\033[1;31m"
-COLOR_GREEN="\033[1;32m"
-COLOR_YELLOW="\033[1;33m"
-COLOR_BLUE="\033[1;34m"
-COLOR_PURPLE="\033[1;35m"
-COLOR_CYAN="\033[1;36m"
-COLOR_GRAY="\033[1;37m"
-COLOR_WHITE="\033[1;38m"
-COLOR_RESET="\033[0m"
+# Log level names
+LOG_LEVELNAMES=('ERROR' 'WARNING' 'INFO' 'DEBUG')
 
-LOG_PROMPT=(
-    "${COLOR_RED}[ERROR]"
-    "${COLOR_YELLOW}[WARNING]"
-    "${COLOR_BLUE}[INFO]"
-    "[DEBUG]" 
-)
+# Global constants definition end }}
+
+# Show log whose level less than this
+log_level=3
+# Default date fmt
+date_fmt='%Y-%m-%d %H:%M:%S'
+# Default log fmt
+log_fmt="[<levelname>] [<asctime>] <message>"
+# Default log color
+log_color=('red' 'yellow' 'blue' '')
+# Support colors
+support_colors='red yellow blue white cyan gray purple green'
 
 # {{ LOG functions start
 
-# Show debug log messages
+# Print log messages
 # $1: Log level
-# $2: C style printf format
+# $2: C style printf fmt
 # $3: C style printf arguments
-function do_debug()
+function do_log()
 {
     local level=$1
-    local format=$2
-    local now=$(date +'%Y-%m-%d %H:%M:%S')
-    local prompt="${LOG_PROMPT[$level]} [$now]"
+    local msg="$2"
+    local fmt="${log_fmt}"
 
     if [ $level -gt $log_level ]; then
         return
-    else
-        shift 2 && printf "${prompt} ${format}${COLOR_RESET}" "$@"
     fi
+
+    fmt="${fmt//<levelname>/${LOG_LEVELNAMES[$level]}}"
+    fmt="${fmt//<asctime>/$(date +"$date_fmt")}"
+    fmt="${fmt//<message>/$msg}"
+
+    shift && ${log_color[level]:-printf} "$fmt" "$@"
 }
 
 function debug_msg()
 {
-    local format=$1
-    shift && do_debug $LOG_DEBUG "$format" "$@"
+    local fmt=$1
+    shift && do_log $LOG_DEBUG "$fmt" "$@"
 }
 
 function info_msg()
 {
-    local format=$1
-    shift && do_debug $LOG_INFO "$format" "$@"
+    local fmt=$1
+    shift && do_log $LOG_INFO "$fmt" "$@"
 }
 
 function warn_msg()
 {
-    local format=$1
-    shift && do_debug $LOG_WARNING "$format" "$@"
+    local fmt=$1
+    shift && do_log $LOG_WARNING "$fmt" "$@"
 }
 
 function error_msg()
 {
-    local format=$1
-    shift && do_debug $LOG_ERROR "$format" "$@"
+    local fmt=$1
+    shift && do_log $LOG_ERROR "$fmt" "$@"
     return 1
 }
 
@@ -85,48 +85,99 @@ function exit_msg()
     exit 1
 }
 
-function set_loglevel()
-{
-    log_level=$1
-}
-
 # LOG functions end }}
 
 # Colorful print start {{
 
 function red()
 {
-    printf "${COLOR_RED}${1}${COLOR_RESET}"
+    printf "\033[1;31m${1}\033[0m" "$@"
 }
 
 function green()
 {
-    printf  "${COLOR_GREEN}${1}${COLOR_RESET}"
+    printf "\033[1;32m${1}\033[0m" "$@"
 }
 
 function gray()
 {
-    printf "${COLOR_GRAY}${1}${COLOR_RESET}"
+    printf "\033[1;37m${1}\033[0m" "$@"
 }
 
 function yellow()
 {
-    printf "${COLOR_YELLOW}${1}${COLOR_RESET}"
+    printf "\033[1;33m${1}\033[0m" "$@"
 }
 
 function blue()
 {
-    printf "${COLOR_BLUE}${1}${COLOR_RESET}"
+    printf "\033[1;34m${1}\033[0m" "$@"
 }
 
 function cyan()
 {
-    printf "${COLOR_CYAN}${1}${COLOR_RESET}"
+    printf "\033[1;36m${1}\033[0m" "$@"
 }
 
 function purple()
 {
-    printf "${COLOR_PURPLE}${1}${COLOR_RESET}"
+    printf "\033[1;35m${1}\033[0m" "$@"
+}
+
+function white()
+{
+    printf "\033[1;38m${1}\033[0m" "$@"
 }
 
 # Colorful print end }}
+
+# {{ Log set functions 
+
+# Set default log level
+function set_loglevel()
+{
+    if echo "$1" | grep -qE "[0-9]+"; then
+        log_level="$1"
+    fi
+}
+
+# Set log format
+function set_logfmt()
+{
+    if [ -n "$1" ]; then
+        log_fmt="$1"
+    fi
+}
+
+# Set date format, see 'man date'
+function set_datefmt()
+{
+    if [ -n "$1" ]; then
+        date_fmt="$1"
+    fi
+}
+
+# Set log colors
+function set_logcolor()
+{
+    local len=$#
+
+    for (( i=0; i<$len; i++ )); do
+        if echo "$support_colors" | grep -wq "$1"
+        then
+            log_color[$i]=$1
+        else
+            log_color[$i]=''
+        fi
+
+        shift
+    done
+}
+
+# Disable colorful log
+function disable_color()
+{
+    set_logcolor '' '' '' ''
+}
+
+# Log set functions }}
--- test.sh
+++ test.sh
@@ -1,11 +1,74 @@
 . ./log.sh
 
-debug_msg "Print debug messages\n"
-info_msg "Print information messages\n"
-warn_msg "Print warning messages\n"
-error_msg "Print error messages\n"
+echo "1. use default log settings"
+echo
 
-debug_msg "Hello, %s\n" "world"
-debug_msg "%15s, %15s\n" "key" "value"
+debug_msg "hello,world\n"
+info_msg 'hello,world\n'
+warn_msg 'hello,world\n'
+error_msg 'hello,world\n'
 
-exit_msg "An unexpected error occurs\n"
+echo
+echo "2. set loglevel to $LOG_INFO"
+echo
+
+set_loglevel $LOG_INFO
+debug_msg "hello,world\n"
+info_msg 'hello,world\n'
+warn_msg 'hello,world\n'
+error_msg 'hello,world\n'
+
+echo
+echo "3. set log fmt to [<asctime> - <levelname>] <message>"
+echo
+
+set_logfmt "[<asctime> - <levelname>] <message>"
+info_msg 'hello,world\n'
+warn_msg 'hello,world\n'
+
+echo
+echo "4. set date fmt to %Y/%m/%d %H:%M:%S"
+echo
+
+set_datefmt "%Y/%m/%d %H:%M:%S"
+info_msg 'hello,world\n'
+warn_msg 'hello,world\n'
+
+echo
+echo "5. set log color to green blue '' yellow"
+echo
+
+set_loglevel $LOG_DEBUG
+set_logcolor green blue '' yellow
+debug_msg "hello,world\n"
+info_msg 'hello,world\n'
+warn_msg 'hello,world\n'
+error_msg 'hello,world\n'
+
+echo
+echo "6. disable color"
+echo
+
+disable_color
+debug_msg "hello,world\n"
+info_msg 'hello,world\n'
+warn_msg 'hello,world\n'
+error_msg 'hello,world\n'
+
+echo
+echo '7. use colorful print'
+echo
+
+red "hello,world\n"
+green "hello,world\n"
+yellow "hello,world\n"
+cyan "hello,world\n"
+gray "hello,world\n"
+purple "hello,world\n"
+blue "hello,world\n"
+white "hello,world\n"
+
+echo
+echo '8. exit script'
+echo
+exit_msg "exit script\n"
